4276c554fe4fac2010a33bf77ec5a00ab744db08
Joshua Clet <63855213+Xiu-Wawa@users.noreply.github.com>
2021-07-14 00:16:43 +0800

    Update Lab1.ino

--- Lab1.ino
+++ Lab1.ino
@@ -2,9 +2,9 @@
 //Print "Hello World"
 
 
-#include <iostream>
-
-int main(){
-    std::cout << "Hello World!";
-    return 0;
+#include <iostream>              // Here iostream is a header file which provides us with input & output streams 
+using namspace std;              // Using namespace std, tells the compiler to use standard namespace
+int main(){                      // main function which holds the executing part of program its return type is int
+    cout << "Hello World!";      // cout<< is used to print anything on screen
+    return 0;                    // return 0 means that the user-defined function is returning false or simply tells us that the program executed successsfully
 }
