ab00d9a4d6361f11046d530a33f9f51e8f0df104
Joshua Clet <63855213+Xiu-Wawa@users.noreply.github.com>
2021-07-13 23:48:26 +0800

    Create Lab1.ino

---
+++ Lab1.ino
@@ -0,0 +1,10 @@
+//Try this exercise for you to familiarize the structure of the C++ Language.
+//Print "Hello World"
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!";
+    return 0;
+}
