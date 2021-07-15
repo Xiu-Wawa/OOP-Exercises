e2ece96aac4362ffc104c912249f6a5151c61924
Dave <d.e.magno13@gmail.com>
2021-07-15 18:27:44 +0800

    Added prerequisites

--- Lab2.cpp
+++ Lab2.cpp
@@ -6,23 +6,27 @@
 // 3. Create 2 methods inside the class (1) to get the input from the user and (2) to contain the formula in calculating the area.
 // 4. The main()  is where the creation of the object and calling of the methods of class Area_of_rectangle.
 
+#include <iostream>
+using namespace std;
+
 
 class Area_of_rectangle {         //______________________________
        private:                                                //|
-             int length, width, area=0;                        //|
+             float length, width, area=0;                        //|
        public:                                                 //|
               void get ()  {                                   //|  
-                    cout<<"Enter length value =";
-                    cin>>length;
-                    cout<<"Enter width value =";             // Creating a class in C++
-                    cin>>width;
-                    }        
+                    cout << "Enter length value =";
+                    cin >> length;
+                    cout << "Enter width value =";             // Creating a class in C++
+                    cin >> width;
+              }
               void result ()  {
-                    area=length * width;                  
                     cout<<"area =";
+                    area=length * width;                  
                     cout<<area;                                //|
-                    }                                          //|   
+              }                                          //|
 };                              //_______________________________|
+
 
 int main() {
        Area_of_rectangle Formula; 
