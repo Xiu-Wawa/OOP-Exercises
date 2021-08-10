25049ce7d993aaf10290d6d67326cd71e3ab8673
Joshua Clet <63855213+Xiu-Wawa@users.noreply.github.com>
2021-08-11 01:37:17 +0800

    Create Lab6.cpp

---
+++ Lab6.cpp
@@ -0,0 +1,88 @@
+# Write a C++ Program having a Class named STUDENT with following members:
+
+# Member Data
+# name                     Name of the student
+# id_number             Student ID
+# total_grade           Assumed total grade out of 500
+# percentage            Percentage of student's total grade
+
+# Member function
+# getDetails()          member function to get student's details
+# putDetails()          member function to print all student's details (from highest percentage to lowest)
+
+
+# Sample output: 
+ # Enter total number of students: 3
+
+ # Enter details of student 1:
+ # Enter name: Karthik
+ # Enter roll number: 1201
+ # Enter total marks out of 500: 456
+
+ # Enter details of student 2:
+ # Enter name: Mahesh
+ # Enter roll number: 1202
+ # Enter total marks out of 500: 398
+
+ # Enter details of student 3:
+ # Enter name: Kiran
+ # Enter roll number: 1203
+ # Enter total marks out of 500: 434
+
+ # Student details:
+ # Name: Karthik, Roll Number: 1201, Total: 456, Percentage: 91.2
+ # Name: Kiran, Roll Number: 1203, Total: 434, Percentage:86.8
+ # Name: Mahesh, Roll Number: 1202, Total: 398, Percentage:79.6
+
+###-----------------------------------------------------------------------------------------------------------------------####
+
+#include <iostream>
+using namespace std;
+# define MAX 10
+
+class STUDENT {
+    private:
+      char name[30];
+      int rollNo;
+      int total;
+      float perc;
+    
+    public:
+    
+      void getDetails() {
+          cout << "Enter name: ";
+          cin >> name;
+          cout << "Enter roll number: ";
+          cin >> rollNo;
+          cout << "Enter total marks out of 500: ";
+          cin >> total;
+          perc=(float)total/500*100;
+      }
+      
+      void putDetails() {
+          cout << "Name:"<< name << ", Roll Number:" << rollNo << ", Total:" << total << ", Percentage:" << perc;
+      }
+};
+      
+int main() {
+    STUDENT std[MAX];
+    int n,loop;
+    cout << "Enter total number of students: ";
+    cin >> n;
+    cout << endl;
+    for (loop=0;loop< n; loop++) {
+        cout << "Enter details of student " << loop+1 << ":\n";
+        std[loop].getDetails();
+        cout << endl;
+    }
+    cout << endl;
+    
+    for(loop=0;loop< n; loop++) {
+        cout << "Details of student " << (loop+1) << ":\n";
+        std[loop].putDetails();
+        cout << endl;
+    }
+ return 0;
+}
+    
+
