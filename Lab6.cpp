92abf7f5dfa5e2f294a4a5bb579e6df40ee1550d
Joshua Clet <63855213+Xiu-Wawa@users.noreply.github.com>
2021-08-15 11:16:34 +0800

    Update Lab6.cpp

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,40 +1,40 @@
-# Write a C++ Program having a Class named STUDENT with following members:
+// Write a C++ Program having a Class named STUDENT with following members:
 
-# Member Data
-# name                     Name of the student
-# id_number             Student ID
-# total_grade           Assumed total grade out of 500
-# percentage            Percentage of student's total grade
+// Member Data
+// name                     Name of the student
+// id_number             Student ID
+// total_grade           Assumed total grade out of 500
+//percentage            Percentage of student's total grade
 
-# Member function
-# getDetails()          member function to get student's details
-# putDetails()          member function to print all student's details (from highest percentage to lowest)
+// Member function
+// getDetails()          member function to get student's details
+// putDetails()          member function to print all student's details (from highest percentage to lowest)
 
 
-# Sample output: 
- # Enter total number of students: 3
+// Sample output: 
+ // Enter total number of students: 3
 
- # Enter details of student 1:
- # Enter name: Karthik
- # Enter roll number: 1201
- # Enter total marks out of 500: 456
+ // Enter details of student 1:
+ // Enter name: Karthik
+ // Enter roll number: 1201
+ // Enter total marks out of 500: 456
 
- # Enter details of student 2:
- # Enter name: Mahesh
- # Enter roll number: 1202
- # Enter total marks out of 500: 398
+ // Enter details of student 2:
+ // Enter name: Mahesh
+ // Enter roll number: 1202
+ // Enter total marks out of 500: 398
 
- # Enter details of student 3:
- # Enter name: Kiran
- # Enter roll number: 1203
- # Enter total marks out of 500: 434
+ // Enter details of student 3:
+ // Enter name: Kiran
+ // Enter roll number: 1203
+ // Enter total marks out of 500: 434
 
- # Student details:
- # Name: Karthik, Roll Number: 1201, Total: 456, Percentage: 91.2
- # Name: Kiran, Roll Number: 1203, Total: 434, Percentage:86.8
- # Name: Mahesh, Roll Number: 1202, Total: 398, Percentage:79.6
+ // Student details:
+ // Name: Karthik, Roll Number: 1201, Total: 456, Percentage: 91.2
+ // Name: Kiran, Roll Number: 1203, Total: 434, Percentage:86.8
+ // Name: Mahesh, Roll Number: 1202, Total: 398, Percentage:79.6
 
-###-------------------------------------------------------------------------------------------------------------------------####
+//-------------------------------------------------------------------------------------------------------------------------//
 
 #include <iostream>
 using namespace std;
